34d96f331e021661bd9bed9872ecb5adb6180f58
jamkerr <kerr0088@gmail.com>
2023-02-02 12:16:31 +0100

    6.3: Save token and user (without pw) to local storage

--- src/app/user-signin-form/user-signin-form.component.ts
+++ src/app/user-signin-form/user-signin-form.component.ts
@@ -32,6 +32,11 @@ export class UserSignInFormComponent implements OnInit {
         this.fetchApiData.userSignIn(this.userData).subscribe((result) => {
             // Logic for a successful user sign in goes here! (To be implemented)
             this.dialogRef.close(); // This will close the modal on success!
+            // Save token to local storage
+            localStorage.setItem('token', result.token);
+            // Remove password from user object, and save remaining user object to local storage
+            const {Password, ...cleanUser} = result.user;
+            localStorage.setItem('user', JSON.stringify(cleanUser));
             console.log(result);
             this.snackBar.open(result, 'OK', {
                 duration: 2000
