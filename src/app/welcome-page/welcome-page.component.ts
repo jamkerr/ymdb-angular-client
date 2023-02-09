771a18572370e8b9f5f6421447ed35ffa646f882
jamkerr <kerr0088@gmail.com>
2023-02-09 10:09:10 +0100

    6.4: Remove unnused functions in welcome card

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserRegistrationFormComponent } from '../user-registration-form/user-registration-form.component';
 import { UserSignInFormComponent } from '../user-signin-form/user-signin-form.component';
-import { MatDialog } from '@angular/material/dialog';
 
 @Component({
     selector: 'app-welcome-page',
@@ -10,22 +9,8 @@ import { MatDialog } from '@angular/material/dialog';
 })
 export class WelcomePageComponent implements OnInit {
 
-    constructor(public dialog: MatDialog) { }
+    constructor() { }
     ngOnInit(): void {
-    }
-    // This is the function that will open the dialog when the signup button is clicked  
-    openUserRegistrationDialog(): void {
-        this.dialog.open(UserRegistrationFormComponent, {
-            // Assigning the dialog a width
-            width: '280px'
-        });
-    }
-    // This is the function that will open the dialog when the signin button is clicked  
-    openUserSignInDialog(): void {
-        this.dialog.open(UserSignInFormComponent, {
-            // Assigning the dialog a width
-            width: '280px'
-        });
     }
 
 }
